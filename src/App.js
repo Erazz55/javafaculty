78d672895ec4643937211d8139901b30463c3815
Oshada Eranga <oshada.e@elysiancrest.com>
2021-02-02 17:18:19 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import bg from './assets/images/bg.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Java Faculty</h1>
+        <div style={{ 
+          backgroundAttachment: 'cover',
+          backgroundPosition: 'cover', 
+          width: '100%',
+          height: '100vh', backgroundColor:'black'}}>
+            
+            <img style={{ 
+          backgroundAttachment: 'cover',
+          backgroundPosition: 'cover', 
+          height: '75vh'}} src={bg}/>
+          {/* <p>Register Now</p> */}
+          <div>
+              <button style={{ textAlign: 'center',
+                               backgroundColor: "#bf2f24",
+                               width: '30%', height: '2.5rem', 
+                               borderRadius: '16px',
+                               fontSize: '1.8rem'}}>Click Here To Register</button>
+          </div>
+          
+        </div>
+        
       </header>
     </div>
   );
